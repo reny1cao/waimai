7e1083397811a5286be1c1a5e1a4229f2ecef82f
allen19981 <50959179+allen19981@users.noreply.github.com>
2020-03-01 14:31:38 -0500

    added Loginpage link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { Home } from './components/Home'
 import { Menu } from './components/Menu'
+import { LogInPage } from './components/LogInPage'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div>
           <Route path="/" exact component={Home} />
           <Route path="/menu" component={Menu} />
+          <Route path="/loginPage" component={LogInPage} />
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Login = () => {
     return (
         <div id="login">
+            <Link className="toLogInPageButton" to={"./../logInPage"}>
             <button>Login</button>
+            </Link>
+            
         </div>
     )
 }
